f3f290f04ac15351ee39cfcb918077f39294cbb3
Meganathan M <megkcalb@gmail.com>
2018-12-24 16:48:34 +0530

    convert aaabbbbcc to a3b4c2

---
+++ compresString.java
@@ -0,0 +1,21 @@
+public class stringCompress{
+
+     public static void main(String []args){
+         String str = "aaabbbbcccc";
+         int i=0;
+         int count = 0;
+         StringBuilder op = new StringBuilder();
+         
+         while(i < str.length() - 1){
+             
+             count++;
+             if(str.charAt(i) != str.charAt(i + 1)){
+                 op.append(str.charAt(i)).append(count);
+                 count = 0;
+               }
+            ++i; 
+         }
+         op.append(str.charAt(i)).append(count);
+        System.out.println("\n compressed : "+op);        
+     }
+}
