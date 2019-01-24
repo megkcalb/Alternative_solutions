31349a97c3598430b48ac72ba35ad51c7cc4de44
Meganathan M <megkcalb@gmail.com>
2019-01-24 15:54:22 +0530

    finding star number ( six point hexagen )
    star Numbers , if 2 , then 13 and if 4 then it is 73.

---
+++ starNum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+        int n ;
+
+        cout << " enter the num : " << endl;
+
+        cin >> n;
+
+        int starNum = ( (6 * n )*( n - 1 ) ) + 1 ;
+
+        cout << " Star Number : " << starNum << endl;
+
+        return 0;
+
+}
